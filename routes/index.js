4a231c14e26fd934088701ca1625d0dfbd11821a
Victor Arza <papu.arza@gmail.com>
2021-05-31 13:38:59 -0400

    new

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,18 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/redirect', (req, res, next) => {
-  console.log(req.query)
-  let data = {
-    "grant_type": "authorization_code",
-    "client_id": "1000.Z2RD3B396MLYW3QR0HV4TKMSIBWKGN",
-    "client_secret": "4e08acb24a427eeec83bfa290e5414a715b5e9aaf4",
-    "redirect_uri": "https://zoho-crm-pulsak.herokuapp.com/final",
-    "code": req.query.code
-  }
-  let final_data = JSON.stringify(data);
-  axios.post(`https://accounts.zoho.eu/oauth/v2/token?`, final_data, {
-    headers: { 'Content-Type': 'application/json' }
+  var bodyFormData = new FormData();
+  bodyFormData.append('grant_type', 'authorization_code');
+  bodyFormData.append('client_id', '1000.Z2RD3B396MLYW3QR0HV4TKMSIBWKGN');
+  bodyFormData.append('client_secret', '4e08acb24a427eeec83bfa290e5414a715b5e9aaf4');
+  bodyFormData.append('redirect_uri', 'https://zoho-crm-pulsak.herokuapp.com/final');
+  bodyFormData.append('code', req.query.code);
+
+  axios.post({
+    method: "post",
+    url: "https://accounts.zoho.eu/oauth/v2/token?",
+    data: bodyFormData,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
   })
     .then(function (response) {
       console.log(response);
