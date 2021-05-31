15fcee7266a977b555b471073e225225a8f7ec2c
Victor Arza <papu.arza@gmail.com>
2021-05-31 12:28:51 -0400

    new

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,22 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/redirect', (req, res, next) => {
-  console.log(req.query.code)
+
   axios.post(`https://accounts.zoho.eu/oauth/v2/token`, {
     grant_type: "authorization_code",
     client_id: "1000.Z2RD3B396MLYW3QR0HV4TKMSIBWKGN",
     client_secret: "4e08acb24a427eeec83bfa290e5414a715b5e9aaf4",
     redirect_uri: "https://zoho-crm-pulsak.herokuapp.com/final",
     code: req.query.code
+  }, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
   })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 router.get('/final', (req, res, next) => {
