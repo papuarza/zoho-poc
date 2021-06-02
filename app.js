5ec6759a3754c0c1693efea929966a9f57985d3b
Victor Arza <papu.arza@gmail.com>
2021-06-02 09:59:33 -0400

    new

--- app.js
+++ app.js
@@ -5,29 +5,9 @@ const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
-const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const Initializer = require( "zcrmsdk/routes/initializer").Initializer;
-const {OAuthToken, TokenType} = require( "zcrmsdk/models/authenticator/oauth_token");
-const UserSignature = require( "zcrmsdk/routes/user_signature").UserSignature;
-const {Logger, Levels} = require( "zcrmsdk/routes/logger/logger");
-const USDataCenter = require( "zcrmsdk/routes/dc/us_data_center").USDataCenter;
-const DBStore = require( "zcrmsdk/models/authenticator/store/db_store").DBStore;
-const FileStore = require( "zcrmsdk/models/authenticator/store/file_store").FileStore;
-const RequestProxy = require("zcrmsdk/routes/request_proxy").RequestProxy;
-const SDKConfigBuilder = require("zcrmsdk/routes/sdk_config_builder").MasterModel;
 
-
-
-mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -41,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-const FormData = require('form-data');
-const qs = require('qs');
 var request = require("request");
 
 
@@ -11,7 +8,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/redirect', (req, res, next) => {
-  console.log(req.query.code)
   var options = {
     method: 'POST',
     url: 'https://accounts.zoho.eu/oauth/v2/token',
@@ -26,7 +22,7 @@ router.get('/redirect', (req, res, next) => {
       grant_type: 'authorization_code',
       client_id: '1000.Z2RD3B396MLYW3QR0HV4TKMSIBWKGN',
       client_secret: '4e08acb24a427eeec83bfa290e5414a715b5e9aaf4',
-      redirect_uri: 'https://zoho-crm-pulsak.herokuapp.com/redirect',
+      redirect_uri: 'http://localhost:3000/redirect',
       code: req.query.code,
       undefined: undefined
     }
@@ -34,13 +30,38 @@ router.get('/redirect', (req, res, next) => {
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
-
-    res.render('papu', {data: body})
+    res.render('token', {data: JSON.parse(body)})
   });
 });
 
-router.get('/final', (req, res, next) => {
-  
+
+router.get('/refresh/:token', (req, res, next) => {
+  var options = {
+    method: 'POST',
+    url: 'https://accounts.zoho.eu/oauth/v2/token',
+    headers:
+    {
+      'Postman-Token': '7323b39b-bbbf-4775-a1c3-e3442bb81d37',
+      'cache-control': 'no-cache',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form:
+    {
+      grant_type: 'refresh_token',
+      client_id: '1000.Z2RD3B396MLYW3QR0HV4TKMSIBWKGN',
+      client_secret: '4e08acb24a427eeec83bfa290e5414a715b5e9aaf4',
+      redirect_uri: 'http://localhost:3000/redirect',
+      refresh_token: req.params.token,
+      undefined: undefined
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    let data = JSON.parse(body);
+    data['refresh_token'] = req.params.token;
+    res.render('refresh', {data})
+  });
 
 });
 
